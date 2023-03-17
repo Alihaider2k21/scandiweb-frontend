91efd6ec3da6608a4720d2e97979b4db37c761d9
Alihaider2k21 <alihaider2k21@gmail.com>
2023-03-17 16:08:49 +0500

    final

--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,26 @@
-import { WebDriver } from "selenium-webdriver";
-import { chrome } from "selenium-webdriver/chrome";
+const webdriver = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
 const options = new chrome.Options();
-const driver = new WebDriver.Builder()
-  .forBrowser("chrome")
-  .setChromeOptions(options)
-  .usingServer("http://localhost:4444/wd/hub")
-  .build();
+
+describe("My Selenium tests", () => {
+  let driver;
+
+  before(async () => {
+    driver = new webdriver.Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .usingServer("http://localhost:4444/wd/hub")
+      .build();
+  });
+
+  after(async () => {
+    await driver.quit();
+  });
+
+  it("should navigate to Google", async () => {
+    await driver.get("https://www.google.com");
+    const title = await driver.getTitle();
+    expect(title).to.equal("Google");
+  });
+});
