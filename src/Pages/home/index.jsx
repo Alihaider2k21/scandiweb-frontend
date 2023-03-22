f892aa0d6bc2082065cef3dc9b69d0ca60244bd9
Alihaider2k21 <alihaider2k21@gmail.com>
2023-03-22 15:10:59 +0500

    get Data before render

--- src/Pages/home/index.jsx
+++ src/Pages/home/index.jsx
@@ -4,12 +4,7 @@ import Navbar from "../../components/navbar";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  const getAllProducts = async () => {
-    const endpoint =
-      "https://alihaiderscandiweb.000webhostapp.com/products/get";
-    const response = await axios.get(endpoint);
-    setProducts(response.data);
-  };
+
   const [checkedProducts, setCheckedProducts] = useState([]);
 
   const deleteProduct = async () => {
@@ -48,68 +43,78 @@ const Home = () => {
   // };
 
   useEffect(() => {
+    const getAllProducts = async () => {
+      const endpoint =
+        "https://alihaiderscandiweb.000webhostapp.com/products/get";
+      const response = await axios.get(endpoint);
+      setProducts(response.data);
+    };
     getAllProducts();
-  }, [products]);
-  return (
-    <div>
-      <Navbar />
-      <div className="jumbotron text-center">
-        <h1>Product list</h1>
-      </div>
-      <div className="container">
-        {/* <form method="post" action="" className="row"> */}
-        <div
-          className="alert alert-success"
-          role="alert"
-          id="message"
-          style={{ display: "none" }}
-        ></div>
-        <div className="col-12 pb-4">
-          <button
-            className="btn btn-danger"
-            id="delete-product-btn"
-            name="delete"
-            onClick={deleteProduct}
-          >
-            MASS DELETE
-          </button>
+  }, []);
+
+  if (!products) {
+    return <div>Loading...</div>;
+  } else
+    return (
+      <div>
+        <Navbar />
+        <div className="jumbotron text-center">
+          <h1>Product list</h1>
         </div>
-        <div className="products col-12 row">
-          {products?.map((product) => (
-            <div className="col-lg-3 pb-4 col-sm-4 col-md-4 col-xs-6">
-              <div className="card" style={{ width: "100%", height: "auto" }}>
-                <div className="card-body">
-                  <div className="form-check-inline">
-                    <label className="form-check-label">
-                      <input
-                        type="checkbox"
-                        className="delete-checkbox"
-                        name="delete-checkbox"
-                        value={product.sku}
-                        onChange={(e) => handleCheckBox(e)}
-                      />
-                    </label>
+        <div className="container">
+          {/* <form method="post" action="" className="row"> */}
+          <div
+            className="alert alert-success"
+            role="alert"
+            id="message"
+            style={{ display: "none" }}
+          ></div>
+          <div className="col-12 pb-4">
+            <button
+              className="btn btn-danger"
+              id="delete-product-btn"
+              name="delete"
+              onClick={deleteProduct}
+            >
+              MASS DELETE
+            </button>
+          </div>
+          <div className="products col-12 row">
+            {products?.map((product) => (
+              <div className="col-lg-3 pb-4 col-sm-4 col-md-4 col-xs-6">
+                <div className="card" style={{ width: "100%", height: "auto" }}>
+                  <div className="card-body">
+                    <div className="form-check-inline">
+                      <label className="form-check-label">
+                        <input
+                          type="checkbox"
+                          className="delete-checkbox"
+                          name="delete-checkbox"
+                          value={product.sku}
+                          onChange={(e) => handleCheckBox(e)}
+                        />
+                      </label>
+                    </div>
+                    <h4 className="card-title">{product.sku}</h4>
+                    <p className="card-text">{product.name}</p>
+                    <p className="card-text">{product.price}$</p>
+                    <p className="card-text">
+                      {product.type === 0
+                        ? "Size: "
+                        : product.type === 1
+                        ? "Weight: "
+                        : "Dimension: "}
+                      {product.attribute}
+                    </p>
                   </div>
-                  <h4 className="card-title">{product.sku}</h4>
-                  <p className="card-text">{product.name}</p>
-                  <p className="card-text">{product.price}$</p>
-                  <p className="card-text">
-                    {product.type === 0
-                      ? "Size: "
-                      : product.type === 1
-                      ? "Weight: "
-                      : "Dimension: "}
-                    {product.attribute}
-                  </p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          {/* </form> */}
         </div>
-        {/* </form> */}
       </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
